29802df974dc2d9a81f3967bd7481990ea3b0898
meligimenez <melinagimenez.dev@gmail.com>
2023-01-13 21:45:30 -0300

    first part

---
+++ README.md
@@ -0,0 +1,3 @@
+## TP N°25 ELEMENTOS CON JAVASCRIPT
+
+>(Elementos)[https://drive.google.com/file/d/18P0EtTIJ2_W77wbBdbPTubnc0EBZuSIO/view]
---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+const qs = (element) => document.querySelector(element);
+const qsa = (element) => document.querySelectorAll(element);
+const $ = (element) => document.getElementById(element);
+
+const main = $('mainContainer');
+const parrafos = qsa('p');
+const subtitulo = qs('.subtitulo');
+const listado = qs('h2 a')
+
+let nombre = prompt('¿Cuál es tu nombre?');
+
+subtitulo.textContent += nombre ? nombre : 'Invitado';
+
+subtitulo.style.textTransform = 'uppercase';
+
+listado.style.color = '#E51B3E';
+
+let respuesta = confirm('¿Querés cambiar el fondo?');
+
+if(respuesta){
+  qs('body').classList.add('fondo');
+}
+
+parrafos.forEach((parrafo, index) => {
+  if(index%2 === 0){
+    parrafo.classList.add('destacadoImpar')
+  }else{
+    parrafo.classList.add('destacadoPar')
+  }
+})
+
+
+
+main.style.display = 'block'
